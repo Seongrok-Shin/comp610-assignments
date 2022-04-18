d02d0a426cce6260643c575c24269485a8802db2
20108539-AUT <82435505+Seongrok-Shin@users.noreply.github.com>
2022-04-19 06:11:23 +1200

    Merge
    Alright

--- comp610-assignment1/src/comp610/assignment1/question1/Element.java
+++ comp610-assignment1/src/comp610/assignment1/question1/Element.java
@@ -3,8 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package comp610.assignment1.hotplate;
+package comp610.assignment1.question1;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,8 +18,9 @@ public class Element implements Runnable {
 
     private final List<Element> neighbours;
     private double currentTemp;
-    private final double heatConstant;
+    private double heatConstant;
     private boolean stopRequested;
+    private Thread thread;
 
     public Element(double startTemp, double heatConstant) {
         this.currentTemp = startTemp;
@@ -26,11 +29,11 @@ public class Element implements Runnable {
     }
 
     public void start() {
-        Thread thread0 = new Thread(this);
-        thread0.start();
+        this.thread = new Thread(this);
+        thread.start();
     }
 
-    public double getTemperature() {
+    public synchronized double getTemperature() {
         return this.currentTemp;
     }
 
@@ -38,20 +41,22 @@ public class Element implements Runnable {
         this.stopRequested = true;
     }
 
+    public void setHeatConstant(double heatConstant) {
+        this.heatConstant = heatConstant;
+    }
+
     @Override
     public void run() {
         double temperatureAvg = 0;
 
         while (!stopRequested) {
-            synchronized (this) {
-                for (int i = 0; i < neighbours.size(); i++) {
-                    temperatureAvg += this.neighbours.get(i).getTemperature();
-                }
-                temperatureAvg /= this.neighbours.size() + 1;
+            for (int i = 0; i < neighbours.size(); i++) {
+                temperatureAvg += this.neighbours.get(i).getTemperature();
             }
+            temperatureAvg /= this.neighbours.size() + 1;
             this.currentTemp += (temperatureAvg - this.currentTemp) * this.heatConstant;
             try {
-                Thread.sleep(1000);
+                Thread.sleep(50);
             } catch (InterruptedException e) {
                 System.err.println(e);
             }
@@ -60,11 +65,24 @@ public class Element implements Runnable {
 
     public void addNeighbour(Element element) {
         System.out.println("Checking the new element added");
-        this.neighbours.add(element);
+        neighbours.add(element);
     }
 
-    public void applyTempToElement(double appliedTemp) {
+    public synchronized void applyTempToElement(double appliedTemp) {
         this.currentTemp += (appliedTemp - this.currentTemp) * this.heatConstant;
+    }
+
+    public void drawElement(Graphics g, int x, int y, int boxWidth, int boxHeight) {
+
+        g.setColor(Color.BLUE);
+        double P = this.getTemperature() / 1000.00;
+        double R = 255.0 * P + 0 * (1.0 - P);
+        double G = 0.0 * P + 0 * (1.0 - P);
+        double B = 0.0 * P + 255 * (1.0 - P);
+        if (P >= 0 && P <= 1) {
+            g.setColor(new Color((int) R, (int) G, (int) B));
+        }
+        g.fillRect(x, y, boxWidth, boxHeight);
     }
 
     public static void main(String[] args) {
@@ -86,12 +104,13 @@ public class Element implements Runnable {
 
             System.out.println("First Element Temperature: " + tempArray[0]);
             System.out.println("Another Element Temperature: " + tempArray[1]);
+            System.out.println("");
 
             try {
                 tempArray[0] = element.getTemperature();
                 tempArray[1] = anotherElement.getTemperature();
-                Thread.sleep(1000);
-                if (Math.abs(tempArray[0] - tempArray[1]) < 0.05) {
+                Thread.sleep(100);
+                if (Math.abs(tempArray[0] - tempArray[1]) < 0.01) {
                     element.requestStop();
                     anotherElement.requestStop();
                 }
---
+++ comp610-assignment1/src/comp610/assignment1/question1/HotPlateGUI.java
@@ -0,0 +1,216 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comp610.assignment1.question1;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ *
+ * @author ssr7324
+ */
+public class HotPlateGUI extends JPanel implements ActionListener, ChangeListener, MouseMotionListener, MouseListener {
+
+    private final JSlider temperatureJSlider;
+    private final JSlider heatConstantJSlider;
+    private final DrawPanel drawPanel;
+
+    private Element[][] dimensionalElement;
+    private final Timer timer;
+
+    private int initializedTemperature = 500;
+    private double initializedHeatConstant;
+
+    private final int ROW = 20;
+    private final int COL = 20;
+
+    private int recX;
+    private int recY;
+
+    private int setX = 0;
+    private int setY = 0;
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        recX = e.getX() / setX;
+        recY = e.getY() / setY;
+
+        try {
+            dimensionalElement[recX][recY].applyTempToElement(initializedTemperature);
+        } catch (ArrayIndexOutOfBoundsException err) {
+            System.err.println(err);
+        }
+        drawPanel.repaint();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    public boolean isElementAtPosition(int getX, int getY, int setRow, int setCol) {
+        return getX >= setRow * 20 && getX <= (setRow * 20) + 20 && getY
+                >= setCol * 20 && getY <= (setCol * 20) + 20;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+        this.mouseDragged(e);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+
+        if (source == timer) {
+            drawPanel.repaint();
+        }
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        JSlider source = (JSlider) e.getSource();
+
+        if (source == heatConstantJSlider) {
+            initializedHeatConstant = source.getValue() / 100.0;
+            for (Element[] dimensionalElement1 : dimensionalElement) {
+                for (int col = 0; col < dimensionalElement.length; col++) {
+                    dimensionalElement1[col].setHeatConstant(initializedHeatConstant);
+                }
+            }
+        }
+        if (source == temperatureJSlider) {
+            initializedTemperature = source.getValue();
+        }
+    }
+
+    private class DrawPanel extends JPanel {
+
+        public DrawPanel() {
+            super.setSize(new Dimension(500, 500));
+            super.setBackground(Color.YELLOW);
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            setX = (getWidth() / ROW);
+            setY = (getHeight() / COL);
+
+            for (int row = 0; row < ROW; row++) {
+                for (int col = 0; col < COL; col++) {
+                    dimensionalElement[row][col].drawElement(g, row * setX,
+                            col * setY, setX - 1,
+                            setY - 1);
+                }
+            }
+        }
+    }
+
+    private void setDimensionalElement() {
+        dimensionalElement = new Element[ROW][COL];
+        for (int row = 0; row < ROW; row++) {
+            for (int col = 0; col < COL; col++) {
+                dimensionalElement[row][col] = new Element(0, 0.05);
+            }
+        }
+    }
+
+    private void addNeighbourToHotPlate() {
+        for (int row = 0; row < dimensionalElement.length; row++) {
+            for (int col = 0; col < dimensionalElement.length; col++) {
+                Element element = new Element(0, 0.05);
+                dimensionalElement[row][col] = element;
+
+                if (row - 1 >= 0 && col - 1 >= 0) {
+                    dimensionalElement[row - 1][col - 1].addNeighbour(element);
+                    dimensionalElement[row][col].addNeighbour(
+                            dimensionalElement[row - 1][col - 1]);
+                } else if (row - 1 >= 0) {
+                    dimensionalElement[row - 1][col].addNeighbour(element);
+                    dimensionalElement[row][col].addNeighbour(
+                            dimensionalElement[row - 1][col]);
+                } else if (row - 1 >= 0 && col < COL - 1) {
+                    dimensionalElement[row - 1][col + 1].addNeighbour(element);
+                    dimensionalElement[row][col].addNeighbour(
+                            dimensionalElement[row - 1][col + 1]);
+                } else if (col - 1 >= 0) {
+                    dimensionalElement[row][col - 1].addNeighbour(element);
+                    dimensionalElement[row][col].addNeighbour(
+                            dimensionalElement[row][col - 1]);
+                }
+                dimensionalElement[row][col].start();
+            }
+        }
+    }
+
+    public HotPlateGUI() {
+        super();
+        super.setLayout(new BorderLayout());
+
+        this.setDimensionalElement();
+        this.addNeighbourToHotPlate();
+
+        JLabel temperatureJLabel = new JLabel("Sets temperature 0 to 1000");
+        temperatureJLabel.setBorder(BorderFactory.createEmptyBorder());
+        temperatureJSlider = new JSlider(0, 1000);
+        temperatureJSlider.setMajorTickSpacing(100);
+        temperatureJSlider.setMajorTickSpacing(10);
+        temperatureJSlider.setPaintTicks(true);
+        temperatureJSlider.addChangeListener(this);
+
+        JLabel heatConstantJLabel = new JLabel("Sets heat constant 0.01 to 1");
+        heatConstantJLabel.setBorder(BorderFactory.createEmptyBorder());
+        heatConstantJSlider = new JSlider(1, 100);
+        heatConstantJSlider.setMajorTickSpacing(10);
+        heatConstantJSlider.setMajorTickSpacing(1);
+        heatConstantJSlider.setPaintTicks(true);
+        heatConstantJSlider.addChangeListener(this);
+
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
+        jPanel.add(temperatureJLabel);
+        jPanel.add(temperatureJSlider);
+        jPanel.add(heatConstantJLabel);
+        jPanel.add(heatConstantJSlider);
+
+        super.add(jPanel, BorderLayout.SOUTH);
+
+        drawPanel = new DrawPanel();
+        drawPanel.addMouseListener(this);
+        drawPanel.addMouseMotionListener(this);
+        super.add(drawPanel, BorderLayout.CENTER);
+
+        timer = new Timer(20, this);
+        timer.start();
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hotplate_Seongrok");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new HotPlateGUI());
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
