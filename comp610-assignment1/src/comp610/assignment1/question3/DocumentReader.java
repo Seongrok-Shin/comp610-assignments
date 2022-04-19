e83de3d3897ab0ffbcebf3f19050fbaba435f000
20108539-AUT <82435505+Seongrok-Shin@users.noreply.github.com>
2022-04-19 22:56:39 +1200

    Set Question 3
    Set Question 3

---
+++ comp610-assignment1/src/comp610/assignment1/question3/DocumentReader.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comp610.assignment1.question3;
+
+/**
+ *
+ * @author ssr7324
+ */
+public class DocumentReader {
+    
+}
